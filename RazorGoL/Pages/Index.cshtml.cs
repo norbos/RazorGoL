8f774099b093a4f4812a8a8bb32afbde453a1649
Norbi Hernández <41130068+norbos@users.noreply.github.com>
2023-07-30 16:16:48 -0400

    Add board related styles

--- RazorGoL/Pages/Index.cshtml.cs
+++ RazorGoL/Pages/Index.cshtml.cs
@@ -1,15 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorGoL.Models;
 
 namespace RazorGoL.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public SimulationState SimulationState { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            SimulationState = SimulationState.GetDefaultState();
         }
 
         public void OnGet()
